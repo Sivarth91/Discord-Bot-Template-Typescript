bf129265069491577da89bc2ac2bb5e94fc72f11
Arthur Sivault <81822795+Sivarth91@users.noreply.github.com>
2023-01-02 11:00:11 +0100

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,8 @@
-import { ChatInputCommandInteraction, Client, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, CommandInteraction, Client, SlashCommandBuilder } from "discord.js";
 
 export interface SlashCommand {
     name: string,
     description: string
     data: SlashCommandBuilder,
-    async execute: (client: Client, interaction: ChatInputCommandInteraction) => Promise<void>
+    async execute: (client: Client, interaction: ChatInputCommandInteraction | CommandInteraction) => Promise<void>
 }
