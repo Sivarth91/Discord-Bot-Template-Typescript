7e6d4bdbf3f1d2ec9efb932001ce2757612aad50
Arthur Sivault <81822795+Sivarth91@users.noreply.github.com>
2023-01-10 18:32:36 +0100

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,7 +2,6 @@ import { ChatInputCommandInteraction, CommandInteraction, Client, SlashCommandBu
 
 export interface SlashCommand {
     name: string,
-    description: string
     data: SlashCommandBuilder,
     async execute: (client: Client, interaction: ChatInputCommandInteraction | CommandInteraction) => Promise<void>
 }
